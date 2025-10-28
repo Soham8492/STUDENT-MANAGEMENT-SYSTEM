5bea46337ef13abc2e9cdc77a1d36687347d4857
Soham8492 <sohamsur96@gmail.com>
2025-10-28 19:59:41 +0530

    Create student .java

---
+++ student .java
@@ -0,0 +1,27 @@
+public class Student {
+    private String id, name;
+    private int marks;
+
+    public Student(String id, String name, int marks) {
+        this.id = id;
+        this.name = name;
+        this.marks = marks;
+    }
+
+    public String getId() { return id; }
+    public String getName() { return name; }
+    public int getMarks() { return marks; }
+    public void setMarks(int marks) { this.marks = marks; }
+
+    public String getGrade() {
+        if (marks >= 90) return "A";
+        else if (marks >= 75) return "B";
+        else if (marks >= 60) return "C";
+        else if (marks >= 40) return "D";
+        else return "F";
+    }
+
+    public String toString() {
+        return id + "," + name + "," + marks + "," + getGrade();
+    }
+}
